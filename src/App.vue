9917b3c415002a9759f6659228972728dfc070bf
LAPTOP-VM1PS42M\Daniel <daniel.hong@essoft.com.tw>
2022-08-22 17:04:49 +0800

    20220822_Vue JS 3 Tutorial for Beginners #5 - The Vue CLI & Bigger Projects (part 2) https://www.youtube.com/watch?v=KM1U6DqZf8M&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=5 6分18秒

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import { def } from '@vue/shared';
+//import { modal } from ".components/Modal.vue";
+import Modal from "./components/Modal.vue";
+
+  export default{
+    name: "App",
+    component: { Modal },
+    data() {
+        return {
+            title: "My first Vue App :)"
+        };
+    },
+    methods: {
+        handleClick() {
+            console.log(this.$refs.name);
+            this.$refs.name.classList.add("active");
+            this.$refs.name.focus();
+        }
+    },
+    components: { Modal }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <h1>My first vue App</h1> -->
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+  <Modal />
 </template>
 
-<style scoped>
-.logo {
+<style>
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -27,5 +41,18 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+} */
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  border-bottom: 5px solid #add;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
